6b22a77641cac8785d94c02aab1619d36eb6ac73
ahashkumar <117459077+ahashkumar@users.noreply.github.com>
2023-12-13 12:30:20 +0530

    Add files via upload

---
+++ day 1/src/Components/login.jsx
@@ -0,0 +1,67 @@
+
+import React, { useState } from 'react';
+import './login.css';
+import ThanksPage from './ThanksPage'; 
+
+const Login = ({ onToggleLogin }) => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [showThanks, setShowThanks] = useState(false); 
+  const toggleMode = () => {
+    setIsLogin((prevMode) => !prevMode);
+  };
+
+  const welcomeMessage = isLogin ? 'Welcome back!' : 'Welcome, new user!';
+
+  const handleLoginComplete = () => {
+    setShowThanks(true);
+  };
+
+  return (
+    <body>
+        <h1>
+              
+        </h1>
+    <div>
+      {!showThanks ? (
+        <div className='container'>
+          <div className="header">
+            <div className="text">
+              {isLogin ? 'CICIC BANK' : 'CICIC BANK'}
+            </div>
+            <div className="underline"></div>
+          </div>
+          <div className="welcome-message">
+            <p>{welcomeMessage}</p>
+          </div>
+          <div className="inputs">
+            <div className="input">
+              <input type="text" placeholder="Username"></input>
+            </div>
+            {!isLogin && (
+              <div className="input">
+                <input type="email" placeholder="Email"></input>
+              </div>
+            )}
+            <div className="input">
+              <input type="password" placeholder="Password"></input>
+            </div>
+          </div>
+          <div className="subyymit-container">
+            <div className="submit" onClick={toggleMode}>
+              {isLogin ? 'SIGN UP' : 'LOGIN'}
+            </div>
+            <div className="submit" onClick={handleLoginComplete}>
+              FINISH
+            </div>
+            
+          </div>
+        </div>
+      ) : (
+        <ThanksPage/>
+      )}
+    </div>
+    </body>
+  );
+};
+
+export default Login;
